cfe1580d726781fc5ac7f06b2d10bf13293066c9
fixxxedpoint <42604451+fixxxedpoint@users.noreply.github.com>
2023-08-17 04:50:20 +0200

    installing all haskell dev-env dependencies through shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,26 @@
 with (import <nixpkgs> { });
 
-haskell.lib.buildStackProject {
+let
+  sources = import ./nix/sources.nix {};
+  pkgs = import sources.nixpkgs {};
+in
+with pkgs; mkShell {
   nativeBuildInputs = [
     perl
     gcc
+    zlib
+    haskell.compiler.ghc945
+    haskellPackages.cabal-install
+    # haskell.packages.ghc945.cabal-install
+    haskell.packages.ghc945.haskell-language-server
+    # haskell.packages.ghc945.apply-refact
+    haskellPackages.apply-refact
+    haskellPackages.hlint
+    haskellPackages.stylish-haskell
+    haskellPackages.hasktags
+    haskellPackages.hoogle
+    haskellPackages.hindent
   ];
 
-  name = "hou";
   src = ./.;
 }
